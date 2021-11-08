0d3865020052e958bfc1b4d7fa06a9746058c187
damian <damiankohut@gmail.com>
2021-11-08 15:10:59 +0000

    finish answers

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function averageInArray(array) {
+    var sum = 0;
+    for(var i = 0; i < array.length;i++){
+        sum += array[i];
+    }
+    return sum / array.length;
+}
+
+console.log(averageInArray([2,2,5,10]))
